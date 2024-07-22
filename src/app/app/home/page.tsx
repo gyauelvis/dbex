4eb778509a5ba5fbf5604f89317c175a10244d9d
gyauelvis <gyauelvis@gmail.com>
2024-07-22 18:53:09 +0000

    tweaks

--- src/app/app/home/page.tsx
+++ src/app/app/home/page.tsx
@@ -17,7 +17,6 @@ import { RecentQueries, AnalyticsComponent, FeedbakCard } from '@/components/hom
 
 import {
   DatabaseIcon,
-  TableIcon,
   TerminalWindowIcon,
   Analytics,
   ServerIcon
@@ -139,37 +138,3 @@ const Page = async() => {
 }
 
 export default Page
-
-
-{/* <div className="relative flex flex-col items-center py-2 gap-4 px-4 font-sans">
-        <div className="homeConnectCard bg-secondary shadow border dark:bg-custom-gradient w-full h-2/5 rounded-lg py-4 px-5 relative">
-          <div className="flex flex-col justify-center h-full gap-2 w-1/2">
-            <div className="hello-text text-muted-foreground">
-              Welcome, Elvis
-              {data?.user?.name}
-            </div>
-            <div className="bold-text font-bold text-4xl leading-tight">
-              Connect to Database With Just a Click
-            </div>
-            <Button className="w-32 flex-row gap-2 text-lg my-2 flex items-center">
-              Connect
-              <ArrowLeftIcon className="size-16 rotate-180 "></ArrowLeftIcon>
-            </Button>
-          </div>
-        </div>
-
-        <div className="doc-website flex justify-between w-full gap-4 h-2/5 relative">
-          <div className="docs bg-secondary shadow-sm dark:bg-custom-gradient w-full h-10/12 rounded-lg py-4 px-8 relative flex flex-col justify-center gap-2 items-center text-center">
-            <div className="bold-text font-bold text-4xl leading-tight">
-              Read Documentation
-            </div>
-          </div>
-          <div className="docs bg-primary-foreground shadow-sm dark:bg-custom-gradient w-full h-10/12 rounded-lg py-4 px-8 relative">
-            <div className="docs bg-primary-foreground shadow-sm dark:bg-custom-gradient w-full h-10/12 rounded-lg py-4 px-8 relative flex flex-col justify-center gap-2 items-center text-center">
-              <div className="bold-text font-bold text-4xl leading-tight">
-                Checkout Website
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
